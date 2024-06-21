97854c68bee25ca3cb78ef4e5c1cdc8b3198d777
Senju002 <rajkaran.mei34@gmail.com>
2024-06-21 12:44:40 +0700

    Product Category Routes and Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -40,6 +41,17 @@ Route::middleware(['auth', 'verified', 'role:ADMIN'])->group(function () {
     Route::get('/customer/{id}/edit', [CustomerController::class, 'edit'])->name('customer.edit');
     Route::post('/customer/{id}/update', [CustomerController::class, 'update'])->name('customer.update');
     Route::post('/customer/delete', [CustomerController::class, 'destroy'])->name('customer.delete');
+
+
+    // !Product Category
+    Route::get('/product-category', [ProductCategoryController::class, "index"])->name('productCategory.index');
+    Route::get('/product-category/add', function () {
+        return Inertia::render('Product Category/Store');
+    })->name('product-category.add');
+    Route::post('/product-category/store', [ProductCategoryController::class, 'store'])->name('productCategory.store');
+    Route::get('/product-category/{id}/edit', [ProductCategoryController::class, 'edit'])->name('productCategory.edit');
+    Route::post('/product-category/{id}/update', [ProductCategoryController::class, 'update'])->name('productCategory.update');
+    Route::post('/product-category/delete', [ProductCategoryController::class, 'destroy'])->name('productCategory.delete');
 });
 
 
